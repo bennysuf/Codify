38163f9fccf41c5ef93ca74767083986bbf7b25d
Benny Sufrin <bennysuf@gmail.com>
2023-05-15 17:43:12 -0500

    add fetch, state, and conditions for developers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,44 +5,55 @@ import Admin from "./Admin";
 import Home from "./Home";
 import Signup from "./Signup";
 import Logout from "./Logout";
+import DevPage from "./DevPage";
+import "@picocss/pico/css/pico.min.css";
 
 export const UserContext = createContext(null);
 
 function App() {
   const [admin, setAdmin] = useState(null);
+  const [devs, setDevs] = useState([]);
 
   let navigate = useNavigate();
 
   useEffect(() => {
     fetch("/admin").then((r) => {
       if (r.ok) {
-        // r.json().then(user => console.log(user))
-        r.json().then((user) => setAdmin(user));
-        // navigate to admin$/${dev_username}
-      } else {
-        // debugger
-        // navigate("/");
-        //no need to do anything
+        r.json().then((user) => {
+          setAdmin(user)
+          // navigate to admin$/${dev_username}
+        });
+     
       }
     });
   }, []);
 
-  console.log("App admin", admin);
+  useEffect(() => {
+    fetch("/developers")
+      .then((r) => r.json())
+      .then(setDevs);
+  }, []);
+
   return (
     <UserContext.Provider
       value={{
         admin,
         setAdmin,
+        devs,
+        setDevs
       }}
     >
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/" element={<Login />} />
         {admin ? (
           <Route path="/admin" element={<Admin />} />
         ) : (
+          <>
           <Route path="/home" element={<Home />} />
+          <Route path="/developers/:devname" element={<DevPage />} />
+          </>
         )}
       </Routes>
     </UserContext.Provider>
