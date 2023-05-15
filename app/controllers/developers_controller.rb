27b0016c51421b3c3f3ca1608cfd158db68de52e
Benny Sufrin <bennysuf@gmail.com>
2023-05-15 15:29:20 -0500

    create all CRUD

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,3 +1,36 @@
 class DevelopersController < ApplicationController
+    skip_before_action :authorize, only: [:index, :show, :create]
+
+    def create
+        dev = Developer.create(dev_params)
+        if dev.valid?
+            session[:dev_id] = dev.id
+            render json: dev, status: :created
+        else
+            render json: {errors: dev.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+ 
+    def show
+        # dev = find_dev
+        dev = Developer.find_by(id: params[:id])
+        render json: dev, status: :ok
+    end
+
+    def index
+        render json: Developer.all, status: :ok
+    end
+
+    def update
+        dev = find_dev
+        dev.update!(dev_params)
+        render json: dev, status: :updated
+    end
+
+    private 
+
+    def dev_params
+        params.permit(:username, :password, :password_confirmation, :email)
+    end
     
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,39 @@
 class ProjectsController < ApplicationController
+    skip_before_action :authorize, only: [:create, :index, :show, :update, :destroy]
+    
+    def create
+        dev = find_dev
+        # dev = Developer.first
+        project = dev.projects.create!(project_params)
+        render json: project, status: :created
+    end
+
+    def update
+        project = Project.find_by_id(params[:id]) 
+        render json: project.update!(project_params), status: :updated
+    end
+
+    def show
+        dev = find_dev
+        # dev = Developer.first
+        projects = dev.projects.all
+        render json: projects, status: :ok
+    end
+
+    def index
+       render json: Project.all
+    end
+
+    def destroy
+        projects = Project.find_by(id: params[:id])
+        projects.destroy
+        render json: {}
+    end
+
+    private
+
+    def project_params
+        params.permit(:title, :url, :description)
+    end
+
 end
