c0768429e4443f3e166f9855b7c1bf6e21e4da2d
Benny Sufrin <bennysuf@gmail.com>
2023-05-15 15:30:30 -0500

    add validations

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -4,5 +4,10 @@ class Developer < ApplicationRecord
     has_many :contact_forms
     has_secure_password
 
-    # validations
+    validates :password, presence: true
+    validates :password_confirmation, presence: true
+    validates :username, presence: true, uniqueness: true
+    # validates_format_of :social_links, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
+    # * to validate, maybe add row for each link? or custom validation to map through array?
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true #! test 
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,18 @@
 class Project < ApplicationRecord
-    has_many :dev_projects
+    has_many :dev_projects, dependent: :destroy
     has_many :developers, through: :dev_projects
 
-    # validations
+    validates :title, presence: true
+    validates :description, presence: true
+    validates_format_of :url, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
+    # Valid ones:
+    # 'www.crowdint.com'
+    # 'crowdint.com'
+    # 'http://crowdint.com'
+    # 'http://www.crowdint.com'
+   
+    # Invalid ones:
+    #  'http://www.crowdint. com'
+    #  'http://fake'
+    #  'http:fake'
 end
