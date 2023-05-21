bd922764b7559526e3ce904ea0c56e0f5bcc3e12
Benny Sufrin <bennysuf@gmail.com>
2023-05-20 23:30:15 -0500

    clean up componen

--- client/src/components/DevPage.js
+++ client/src/components/DevPage.js
@@ -1,33 +1,29 @@
 import { useEffect, useContext, useState } from "react";
-import { Route, Routes, Link } from "react-router-dom";
 import ProjectCard from "./ProjectCard";
-import AboutPage from "./AboutPage";
-import { UserContext } from "./App";
+// import { UserContext } from "./App";
 
 export default function DevPage({ dev }) {
   const [currentDev, setCurrentDev] = useState({ username: "Loading..." });
-
-  // console.log("devPage dev", dev);
 
   useEffect(() => {
     setCurrentDev(dev);
   }, []);
 
   const projects = dev.projects.map((project) => (
-    <ProjectCard project={project} />
+    <ProjectCard key={project.id} project={project} />
   ));
-  console.log("devpage resume", dev.profile);
+
   return (
     <>
       <nav aria-label="breadcrumb">
         <ul>
-          <li>
+          <li key="about">
             <a href={"/about?developers=" + dev.username}>About</a>
           </li>
-          <li>
+          <li key="contact">
             <a href={"/contact?developers=" + dev.username}>Contact</a>
           </li>
-          <li>
+          <li key="resume">
             <a
               href={"https://" + dev.profile.resume}
               target="_blank"
@@ -41,12 +37,6 @@ export default function DevPage({ dev }) {
       <h3>DevPage</h3>
       {currentDev?.username}
       {projects}
-      <Routes>
-        <Route
-          path={"/about?developers=" + dev.username}
-          elements={AboutPage}
-        />
-      </Routes>
     </>
   );
 }
