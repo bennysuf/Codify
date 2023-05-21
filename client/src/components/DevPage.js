a3b178fc02f6c5bd2806d4dc03a425bf6c7ffed4
Benny Sufrin <bennysuf@gmail.com>
2023-05-21 00:14:19 -0500

    add userContext, add DevNavBar

--- client/src/components/DevPage.js
+++ client/src/components/DevPage.js
@@ -1,41 +1,19 @@
-import { useEffect, useContext, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+import { UserContext } from "./App";
 import ProjectCard from "./ProjectCard";
-// import { UserContext } from "./App";
+import DevNavBar from "./DevNavBar";
 
-export default function DevPage({ dev }) {
-  const [currentDev, setCurrentDev] = useState({ username: "Loading..." });
-
-  useEffect(() => {
-    setCurrentDev(dev);
-  }, []);
-
-  const projects = dev.projects.map((project) => (
+export default function DevPage() {
+  const { currentDev } = useContext(UserContext);
+  
+  const projects = currentDev[0].projects.map((project) => (
     <ProjectCard key={project.id} project={project} />
   ));
 
   return (
     <>
-      <nav aria-label="breadcrumb">
-        <ul>
-          <li key="about">
-            <a href={"/about?developers=" + dev.username}>About</a>
-          </li>
-          <li key="contact">
-            <a href={"/contact?developers=" + dev.username}>Contact</a>
-          </li>
-          <li key="resume">
-            <a
-              href={"https://" + dev.profile.resume}
-              target="_blank"
-              rel="noreferrer"
-            >
-              Resume
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <DevNavBar />
       <h3>DevPage</h3>
-      {currentDev?.username}
       {projects}
     </>
   );
