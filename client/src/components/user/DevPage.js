7b9112530d6c5ff4f15dea361d98680a501fee97
Benny Sufrin <bennysuf@gmail.com>
2023-05-22 21:55:39 -0500

    clean up format

--- client/src/components/user/DevPage.js
+++ client/src/components/user/DevPage.js
@@ -5,11 +5,11 @@ import DevNavBar from "./DevNavBar";
 
 export default function DevPage() {
   const { currentDev } = useContext(UserContext);
-  
+
   const projects = currentDev[0].projects.map((project) => (
     <div key={project.url}>
-    <ProjectCard key={project.id} project={project} />
-    <br/>
+      <ProjectCard key={project.id} project={project} />
+      <br />
     </div>
   ));
 
