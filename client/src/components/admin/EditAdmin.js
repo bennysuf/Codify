ab90d659cb04d30495ad0ef651d7171e6fe6cf2c
Benny Sufrin <bennysuf@gmail.com>
2023-05-22 22:01:09 -0500

    add imports, fetches, inputs

--- client/src/components/admin/EditAdmin.js
+++ client/src/components/admin/EditAdmin.js
@@ -1,45 +1,84 @@
-import { useState, useContext } from "react"
-import { UserContext } from "../App"
-//import Textarea 
+import { useState, useContext } from "react";
+import { UserContext } from "../App";
 
-export default function EditAdmin(){
-    const { admin } = useContext(UserContext)
+export default function EditAdmin() {
+  const { admin, navigate, setAdmin } = useContext(UserContext);
 
-    const {about, social_links, resume} = admin.profile
-    
-    //TODO: edit public profile/admin in general, how to have two fetches? use same update value? or create new one fetch and update value? or create new component?
+  const { about, social_links } = admin.profile;
+  const { about, social_links, resume } = admin.profile;
 
-    const [aboutPage, setAboutPage] = useState(about)
-    const [resumeUrl, setResumeUrl] = useState(resume)
-    const [socialLinks, setSocialLinks] = useState(social_links)
+  //TODO: edit public profile/admin in general, how to have two fetches? use same update value? or create new one fetch and update value? or create new component?
 
-    function handleAboutChange(e) {
-       e.preventDefault();
-        setAboutPage(e.target.value);
-    }
-    function handleResumeChange(e) {
-       e.preventDefault();
-        setResumeUrl(e.target.value);
-    }
-    function handleSocialChange(e) {
-       e.preventDefault();
-        setSocialLinks(e.target.value);
-    }
+  const [aboutPage, setAboutPage] = useState(about);
+  const [resumeUrl, setResumeUrl] = useState(resume);
+  const [socialLinks, setSocialLinks] = useState(social_links);
+  // const [aboutPage, setAboutPage] = useState("");
+  // const [resumeUrl, setResumeUrl] = useState("");
+  // const [socialLinks, setSocialLinks] = useState("");
 
-    function onSubmit(e) {
-        // const update = {
-        // about: aboutPage,
-        // social_links: socialLinks,
-        // resume: resumePage
-        // }
-        // fetch("/profiles/:id", {
-        // method: "PATCH",
-        // 
-        // })
-    }
+  function handleAboutChange(e) {
+    e.preventDefault();
+    setAboutPage(e.target.value);
+  }
+  function handleResumeChange(e) {
+    e.preventDefault();
+    setResumeUrl(e.target.value);
+  }
+  function handleSocialChange(e) {
+    e.preventDefault();
+    setSocialLinks(e.target.value);
+  }
 
-    return (
-        // input form 
-        ""
-    )
+  function handleSubmit(e) {
+    e.preventDefault();
+    const update = {
+      about: aboutPage,
+      social_links: socialLinks,
+      resume: resumeUrl,
+    };
+    fetch(`/profiles/${admin.id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(update),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((d) => {
+          setAdmin(d);
+          navigate("/admin");
+        });
+      }
+    });
+  }
+
+  //TODO: make value ternary if value exists
+
+  return (
+    <>
+      <h1>Edit admin</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="input">
+          <input
+            placeholder="Resume link"
+            value={resumeUrl}
+            onChange={handleResumeChange}
+          />
+          <input
+            placeholder="social"
+            value={socialLinks}
+            onChange={handleSocialChange}
+          />
+          <textarea
+            placeholder="About page"
+            value={aboutPage}
+            onChange={handleAboutChange}
+            style={{ height: "100px" }}
+          />
+        </div>
+        <button type="submit" className="button">
+          Submit
+        </button>
+        <br />
+      </form>
+    </>
+  );
 }
