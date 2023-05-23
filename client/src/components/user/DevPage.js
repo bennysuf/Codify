14e7af4cbad716aad6c7c5d51f5b5b2223e7b235
Benny Sufrin <bennysuf@gmail.com>
2023-05-22 19:58:04 -0500

    update elements

--- client/src/components/user/DevPage.js
+++ client/src/components/user/DevPage.js
@@ -1,13 +1,16 @@
 import { useContext } from "react";
 import { UserContext } from "../App";
-import ProjectCard from "./ProjectCard";
+import ProjectCard from "../ProjectCard";
 import DevNavBar from "./DevNavBar";
 
 export default function DevPage() {
   const { currentDev } = useContext(UserContext);
   
   const projects = currentDev[0].projects.map((project) => (
+    <div key={project.url}>
     <ProjectCard key={project.id} project={project} />
+    <br/>
+    </div>
   ));
 
   return (
