aefa85909c1c899ecb5d2165c4a8e228cd88cde1
Benny Sufrin <bennysuf@gmail.com>
2023-05-22 22:00:06 -0500

    add imports, props, fetches, inputs

--- client/src/components/admin/EditProject.js
+++ client/src/components/admin/EditProject.js
@@ -1,68 +1,100 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { useParams } from "react-router-dom";
+import { UserContext } from "../App";
 
-export default function EditProject() {
-  const [title, setTitle] = useState();
-  const [description, setDescription] = useState();
-  const [url, setUrl] = useState();
+export default function EditProject({ project }) {
+  const { projects, setProjects, navigate } = useContext(UserContext);
 
-  function handleTitleChange(e) {
+  const { projectId } = useParams();
+
+  const currentProject = projects.filter(
+    (project) => project.id === parseInt(projectId)
+  );
+
+  const { description, title, url } = currentProject[0];
+
+  const [newTitle, setNewTitle] = useState(title);
+  const [newDescription, setNewDescription] = useState(description);
+  const [newUrl, setNewUrl] = useState(url);
+
+  function handleNewTitleChange(e) {
     e.preventDefault();
-    setTitle(e.target.value);
+    setNewTitle(e.target.value);
   }
 
-  function handleDescriptionChange(e) {
+  function handleNewDescriptionChange(e) {
     e.preventDefault();
-    setDescription(e.target.value);
+    setNewDescription(e.target.value);
   }
 
-  function handleUrlChange(e) {
+  function handleNewUrlChange(e) {
     e.preventDefault();
-    setUrl(e.target.value);
+    setNewUrl(e.target.value);
   }
 
   function handleSubmit(e) {
     e.preventDefault();
     const update = {
-      title: title,
-      url: url,
-      description: description,
+      title: newTitle,
+      url: newUrl,
+      description: newDescription,
     };
-    // fetch("/projects/:id", {
-    // method: "PATCH",
-    // headers: {"Content-Type": "application/json"},
-    // body: JSON.stringify(update)
-    // })
-    // .then(r => {
-    // if(r.ok){
-    // r.json().then(() => {
-    // const updated = projects.map(project => {
-    // return project.id !== item.id ? project : item;
-    // })
-    // setProjects(updated)
-    // })
-    // }
-    // })
+    fetch(`/projects/${projectId}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(update),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((d) => {
+          const updated = projects.map((project) => {
+            return project.id !== d.id ? project : d;
+          });
+          setProjects(updated);
+          navigate("/admin/projects-page");
+        });
+      }
+    });
   }
 
-  function handleDelete(){
-//     fetch("/projects/:id", { 
-//         method: "DELETE"
-//   })
-//   .then(r => r.json())
-//   .then(() => {
-//     const updated = projects.filter(project => project.id !== item.id)
-//     setProjects(updated)
-//   })
-}
-
-
-  //! need setProjects here and projects, do i add route to Admin, prop drill, or create new useContext?
+  function handleDelete(item) {
+    fetch(`/projects/${projectId}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => {
+        const updated = projects.filter((project) => project.id !== item.id);
+        setProjects(updated);
+        navigate("/admin/projects-page");
+      });
+  }
 
   return (
-    //input fields
-    <form onSubmit={handleSubmit}>
-    <button type="submit">Submit</button>
-    <button type="button" onClick={handleDelete}>Delete project</button>
-  </form>
+    <>
+      <h3>Edit projects page</h3>
+      <form onSubmit={handleSubmit}>
+        <div className="input">
+          <input
+            placeholder="Title"
+            value={newTitle}
+            onChange={handleNewTitleChange}
+          />
+          <input
+            placeholder="Url"
+            value={newUrl}
+            onChange={handleNewUrlChange}
+          />
+          <textarea
+            placeholder="Description"
+            value={newDescription}
+            onChange={handleNewDescriptionChange}
+            style={{ height: "100px" }}
+          />
+        </div>
+        <button type="submit">Submit</button>
+        <button type="button" onClick={() => handleDelete(project)}>
+          Delete project
+        </button>
+      </form>
+    </>
   );
 }
