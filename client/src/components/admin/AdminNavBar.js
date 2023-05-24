922499a8421990877af39e2d143a341613230070
Benny Sufrin <bennysuf@gmail.com>
2023-05-24 14:17:55 -0500

    add dynamic route

--- client/src/components/admin/AdminNavBar.js
+++ client/src/components/admin/AdminNavBar.js
@@ -1,24 +1,25 @@
+export default function AdminNavBar({ profile }) {
 
-export default function AdminNavBar(){
+  const param = profile ? "edit-admin" : "create-profile";
+ 
+  //if current page, make <a> class="secondary" else ""
 
-     //if current page, make <a> class="secondary" else "" 
-
-    return (
-        <nav aria-label="breadcrumb">
-        <ul style={{ marginLeft: "3%" }}>
-          <li key="about">
-            <a href="/admin/edit-admin" >Edit admin</a>
-          </li>
-          <li key="new-project">
-            <a href="/admin/new-project">New project</a>
-          </li>
-          <li key="projects-page">
-            <a href="/admin/projects-page">Projects</a>
-          </li>
-          <li key="admin">
-            <a href="/admin">Home</a>
-          </li>
-        </ul>
-      </nav>
-    )
+  return (
+    <nav aria-label="breadcrumb">
+      <ul style={{ marginLeft: "3%" }}>
+        <li key="about">
+          <a href={"/admin/" + param}>Edit admin</a>
+        </li>
+        <li key="new-project">
+          <a href="/admin/new-project">New project</a>
+        </li>
+        <li key="projects-page">
+          <a href="/admin/projects-page">Projects</a>
+        </li>
+        <li key="admin">
+          <a href="/admin">Home</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
