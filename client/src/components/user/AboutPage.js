2a7ba420af55235e7ed23238599ed7a731560870
Benny Sufrin <bennysuf@gmail.com>
2023-05-24 14:16:13 -0500

    update styling

--- client/src/components/user/AboutPage.js
+++ client/src/components/user/AboutPage.js
@@ -3,16 +3,27 @@ import { UserContext } from "../App";
 import DevNavBar from "./DevNavBar";
 
 export default function AboutPage() {
+  // const [num, setNum] = useState(100)
   const { currentDev } = useContext(UserContext);
 
   const { profile } = currentDev[0];
+
+
+  //ternary, if profile.about.length 
+  // if(profile.about.length > 100) {
+  //   setNum(300)
+  // } else if(profile.AboutPage.length > 1000) {
+  //   setNum(700)
+  // } else {
+
+  // }
 
   return (
     <>
       <DevNavBar />
       <article>
         <header>About Me</header>
-        <textarea defaultValue={profile.about}/>
+        <textarea defaultValue={profile ? profile.about : ""} style={{ height: `500px` }}/>
       </article>
     </>
   );
