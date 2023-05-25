0d8a72aa488fb86e0c0829ecdf207283691060d1
Benny Sufrin <bennysuf@gmail.com>
2023-05-25 17:49:53 -0500

    add states, destructoring, fetch for developer, and inputs

--- client/src/components/admin/EditAdmin.js
+++ client/src/components/admin/EditAdmin.js
@@ -4,15 +4,16 @@ import { UserContext } from "../App";
 export default function EditAdmin() {
   const { admin, navigate, setAdmin } = useContext(UserContext);
 
-  const { about, social_links, resume } = admin.profile;
-
-  //TODO: edit public profile/admin in general, how to have two fetches? use same update value? or create new one fetch and update value? or create new component?
-
+  const { username, public_profile, profile } = admin;
+  const { about, social_links, resume } = profile;
 
   const [aboutPage, setAboutPage] = useState(about);
   const [resumeUrl, setResumeUrl] = useState(resume);
   const [socialLinks, setSocialLinks] = useState(social_links);
- 
+  const [newUsername, setNewUsername] = useState(username);
+  const [publicProfile, setPublicProfile] = useState(public_profile);
+  const [errors, setErrors] = useState("");
+
   function handleAboutChange(e) {
     e.preventDefault();
     setAboutPage(e.target.value);
@@ -25,10 +26,15 @@ export default function EditAdmin() {
     e.preventDefault();
     setSocialLinks(e.target.value);
   }
+  function handleUsernameChange(e) {
+    e.preventDefault();
+    setNewUsername(e.target.value);
+  }
 
   function handleSubmit(e) {
+    // ? can i update developers and in backend update dev.profile?
     e.preventDefault();
-    const update = {
+    const updateProfile = {
       about: aboutPage,
       social_links: socialLinks,
       resume: resumeUrl,
@@ -36,40 +42,86 @@ export default function EditAdmin() {
     fetch(`/profiles/${admin.id}`, {
       method: "PATCH",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(update),
+      body: JSON.stringify(updateProfile),
     }).then((r) => {
       if (r.ok) {
         r.json().then((d) => {
-          setAdmin(d);
-          navigate("/admin");
+          const updateAdmin = {
+            username: newUsername,
+            public_profile: publicProfile,
+          };
+          fetch(`/developers/${d.id}`, {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(updateAdmin),
+          }).then((r) => {
+            if (r.ok) {
+              r.json().then((d) => setAdmin(d));
+              navigate("/admin");
+            } else {
+              r.json().then((err) => {
+                setErrors(err.error);
+                window.scrollTo(0, 0);
+              });
+            }
+          });
         });
       }
     });
   }
 
-  //TODO: make value ternary if value exists
-
   return (
     <>
-      <h1>Edit admin</h1>
       <form onSubmit={handleSubmit}>
         <div className="input">
-          <input
-            placeholder="Resume link"
-            value={resumeUrl}
-            onChange={handleResumeChange}
-          />
-          <input
-            placeholder="social"
-            value={socialLinks}
-            onChange={handleSocialChange}
-          />
-          <textarea
-            placeholder="About page"
-            value={aboutPage}
-            onChange={handleAboutChange}
-            style={{ height: "100px" }}
-          />
+          <br />
+          <label style={{ marginLeft: "3%", width: "fit-content" }}>
+            {/* //TODO: how to have the onChange effect only the checkbox? */}
+            <input
+              type="checkbox"
+              id="terms"
+              name="terms"
+              checked={publicProfile}
+              value={publicProfile.toString()}
+              onChange={() => setPublicProfile(!publicProfile)}
+            />
+            Public profile {publicProfile ? "ON" : "OFF"}
+          </label>
+          <br />
+          <h3 style={{ marginLeft: "3%" }}>{errors}</h3>
+          <label>
+            Username
+            <input
+              placeholder="username"
+              value={newUsername}
+              onChange={handleUsernameChange}
+            />
+          </label>
+          <label>
+            Resume URL
+            <input
+              placeholder="Resume link"
+              value={resumeUrl}
+              onChange={handleResumeChange}
+            />
+          </label>
+          <label>
+            Social links
+            <input
+              placeholder="Social links"
+              value={socialLinks ? socialLinks : ""}
+              onChange={handleSocialChange}
+            />
+          </label>
+          <label>
+            About page
+            <textarea
+              placeholder="About page"
+              value={aboutPage}
+              onChange={handleAboutChange}
+              style={{ height: "500px" }}
+            />
+          </label>
         </div>
         <button type="submit" className="button">
           Submit
