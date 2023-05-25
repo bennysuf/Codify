e0dd20012441f5f506622ebf3d0289680a88da17
Benny Sufrin <bennysuf@gmail.com>
2023-05-25 17:50:19 -0500

    update routes

--- client/src/components/admin/AdminNavBar.js
+++ client/src/components/admin/AdminNavBar.js
@@ -1,23 +1,22 @@
 export default function AdminNavBar({ profile }) {
-
   const param = profile ? "edit-admin" : "create-profile";
- 
-  //if current page, make <a> class="secondary" else ""
+
+  // TODO: if current page, make <a> class="secondary" else ""
 
   return (
     <nav aria-label="breadcrumb">
       <ul style={{ marginLeft: "3%" }}>
-        <li key="about">
-          <a href={"/admin/" + param}>Edit admin</a>
-        </li>
-        <li key="new-project">
-          <a href="/admin/new-project">New project</a>
+        <li key="admin">
+          <a href="/admin">Home</a>
         </li>
         <li key="projects-page">
           <a href="/admin/projects-page">Projects</a>
         </li>
-        <li key="admin">
-          <a href="/admin">Home</a>
+        <li key="new-project">
+          <a href="/admin/new-project">New project</a>
+        </li>
+        <li key="about">
+          <a href={"/admin/" + param}>Edit profile</a>
         </li>
       </ul>
     </nav>
