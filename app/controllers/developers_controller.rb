60f1138d5dfa57fb9380a7d505690dc6780286f4
Benny Sufrin <bennysuf@gmail.com>
2023-05-25 17:44:39 -0500

    update the update method to get around validations

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,5 +1,5 @@
 class DevelopersController < ApplicationController
-    skip_before_action :authorize, only: [:index, :show, :create, :update]
+    skip_before_action :authorize, only: [:index, :show, :create, :update, :destroy]
 
     def create
         dev = Developer.create(
@@ -9,6 +9,7 @@ class DevelopersController < ApplicationController
             email: params[:email],
             public_profile: false
             # dev_params
+            #? how to have strong params + public_profile? 
             )
         if dev.valid?
             session[:dev_id] = dev.id
@@ -24,19 +25,41 @@ class DevelopersController < ApplicationController
     end
 
     def index
-        render json: Developer.all.where(public_profile: true), status: :ok
+        render json: Developer.all, status: :ok
+        # render json: Developer.where(public_profile: true), status: :ok
     end
 
     def update
         dev = find_dev
-        dev.update!(dev_params)
-        render json: dev, status: :accepted
+        dev.tap do |u|
+            if params[:username] == ""
+                render json: {error: "Please add username"}, status: :unprocessable_entity
+            elsif dev.username == params[:username]
+                # ? how can i combine this with no other error? 
+                u.public_profile = params[:public_profile]
+                u.save(validate: false)
+                render json: dev, status: :accepted
+            elsif Developer.find_by(username: params[:username]) != nil
+                render json: {error: "Username already exists"}, status: :unprocessable_entity
+            else 
+                u.username = params[:username]
+                u.public_profile = params[:public_profile]
+                u.save(validate: false)
+                render json: dev, status: :accepted
+            end
+        end
+    end
+
+    def destroy
+        dev = Developer.find(params[:id])
+        dev.destroy
+        render json: {}, status: :ok
     end
 
     private 
 
     def dev_params
-        params.permit(:username, :password, :password_confirmation, :email, :public_profile)
+        params.require(:developer).permit(:username, :password, :password_confirmation, :email, :public_profile)
     end
     
 end
