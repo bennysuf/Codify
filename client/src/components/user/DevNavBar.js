6de86f82957868f52b36be873c9a2e0b58addcaa
Benny Sufrin <bennysuf@gmail.com>
2023-05-30 14:17:28 -0400

    add ternarys to links

--- client/src/components/user/DevNavBar.js
+++ client/src/components/user/DevNavBar.js
@@ -1,39 +1,48 @@
 import { useContext } from "react";
 import { UserContext } from "../App";
 
-export default function DevNavBar(){
-    const { currentDev } = useContext(UserContext);
+export default function DevNavBar() {
+  const { currentDev } = useContext(UserContext);
 
-    const { username, profile } = currentDev[0]
+  const { username, resume, about } = currentDev[0];
+  // const { about, resume } = profile;
 
-    // TODO: if current page, make <a> class="secondary" else "" 
+  // TODO: if current page, make <a> class="secondary" else ""
 
-    return (
-        <nav aria-label="breadcrumb">
-        <ul style={{ marginLeft: "3%" }}>
-          <li key="dev-page">
-            <a href={"/developer?developers=" + username}>{username}'s page</a> 
-            {/* // TODO: make username first letter capital */}
-          </li>
+  return (
+    <nav aria-label="breadcrumb">
+      <ul style={{ marginLeft: "3%" }}>
+        <li key="dev-page">
+          <a href={"/developer?developers=" + username}>{username}'s page</a>
+          {/* // TODO: make username first letter capital */}
+        </li>
+        {about === "" ? (
+          <></>
+        ) : (
           <li key="about">
             <a href={"/about?developers=" + username}>About</a>
           </li>
-          <li key="contact">
-            <a href={"/contact?developers=" + username}>Contact</a>
-          </li>
+        )}
+        <li key="contact">
+          <a href={"/contact?developers=" + username}>Contact</a>
+        </li>
+        {resume === "" ? (
+          <></>
+        ) : (
           <li key="resume">
             <a
-              href={"https://" + profile.resume}
+              href={"https://" + resume}
               target="_blank"
               rel="noreferrer"
             >
               Resume
             </a>
           </li>
-          <li key="home">
-            <a href="/home">Home</a>
-          </li>
-        </ul>
-      </nav>
-    )
+        )}
+        <li key="home">
+          <a href="/home">Home</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
