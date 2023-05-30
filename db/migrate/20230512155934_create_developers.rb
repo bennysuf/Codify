4e2ec404a106cfd6d882295abbbc4f2bc6d3b915
Benny Sufrin <bennysuf@gmail.com>
2023-05-30 14:13:16 -0400

    add new migration, remove old one

--- db/migrate/20230512155934_create_developers.rb
+++ db/migrate/20230512155934_create_developers.rb
@@ -3,7 +3,10 @@ class CreateDevelopers < ActiveRecord::Migration[7.0]
     create_table :developers do |t|
       t.string :username
       t.string :password_digest
-      t.boolean :public_profile
+      t.boolean :public_profile, :default => false
+      t.text :about, :default => ""
+      t.text :social_links, :default => ""
+      t.string :resume, :default => ""
       t.string :email
 
       t.timestamps
