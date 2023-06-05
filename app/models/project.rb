f29df600d854db3da89465b0d24b7f7c0fad0eac
Benny Sufrin <bennysuf@gmail.com>
2023-06-04 20:27:24 -0400

    add data type serializer

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,9 +2,11 @@ class Project < ApplicationRecord
     has_many :dev_projects, dependent: :destroy
     has_many :developers, through: :dev_projects
 
+    serialize :url, Hash
+
     validates :title, presence: true
     validates :description, presence: true
-    validates_format_of :url, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
+    # validates_format_of :url, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
     # Valid ones:
     # 'www.crowdint.com'
     # 'crowdint.com'
