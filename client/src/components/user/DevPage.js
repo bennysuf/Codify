335559782d694dff293a0d806b8d73c90d7f5fa8
Benny Sufrin <bennysuf@gmail.com>
2023-06-04 20:17:06 -0400

    move routes into component, and projects out

--- client/src/components/user/DevPage.js
+++ client/src/components/user/DevPage.js
@@ -1,23 +1,20 @@
-import { useContext } from "react";
-import { UserContext } from "../App";
-import ProjectCard from "../ProjectCard";
+import { Route, Routes } from "react-router-dom";
 import DevNavBar from "./DevNavBar";
+import AboutPage from "./AboutPage";
+import ContactForm from "./ContactForm";
+import DevProjectPage from "./DevProjectPage";
 
 export default function DevPage() {
-  const { currentDev } = useContext(UserContext);
-
-  const projects = currentDev[0].projects.map((project) => (
-    <div key={project.url}>
-      <ProjectCard key={project.id} project={project} />
-      <br />
-    </div>
-  ));
 
   return (
     <>
       <DevNavBar />
       <h3>DevPage</h3>
-      {projects}
+      <Routes>
+        <Route path="about" element={<AboutPage />} />
+        <Route path="contact" element={<ContactForm />} />
+        <Route path="projects" element={<DevProjectPage />} />
+      </Routes>
     </>
   );
 }
