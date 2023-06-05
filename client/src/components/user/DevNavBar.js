7d81035c871a61433d5332319f667d1c0beb3d3b
Benny Sufrin <bennysuf@gmail.com>
2023-06-04 20:18:24 -0400

    add nav to projects and socials

--- client/src/components/user/DevNavBar.js
+++ client/src/components/user/DevNavBar.js
@@ -4,21 +4,20 @@ import { UserContext } from "../App";
 export default function DevNavBar() {
   const { currentDev } = useContext(UserContext);
 
-  const { username, resume, about, social_links } = currentDev[0];
-  // const { about, resume } = profile;
+  const { username, resume, about, social_links, projects } = currentDev[0];
 
   const path = window.location.pathname;
 
   return (
     <>
       <nav>
-      {/* <nav aria-label="breadcrumb"> */}
+        {/* <nav aria-label="breadcrumb"> */}
         <ul style={{ marginLeft: "5%" }}>
-        <li >
+          <li>
             <strong>Codify</strong>
           </li>
         </ul>
-        <ul style={{marginRight: "3%"}}>
+        <ul style={{ marginRight: "3%" }}>
           <li key="dev-page">
             <a
               href={"/developer?developers=" + username}
@@ -33,17 +32,29 @@ export default function DevNavBar() {
           ) : (
             <li key="about">
               <a
-                href={"/about?developers=" + username}
-                className={path === "/about" ? "secondary" : ""}
+                href={"/developer/about?developers=" + username}
+                className={path === "/developer/about" ? "secondary" : ""}
               >
                 About
               </a>
             </li>
           )}
+          {!projects[0] ? (
+            <></>
+          ) : (
+            <li key="projects">
+              <a
+                href={"/developer/projects?developers=" + username}
+                className={path === "/developer/projects" ? "secondary" : ""}
+              >
+                Projects
+              </a>
+            </li>
+          )}
           <li key="contact">
             <a
-              href={"/contact?developers=" + username}
-              className={path === "/contact" ? "secondary" : ""}
+              href={"/developer/contact?developers=" + username}
+              className={path === "/developer/contact" ? "secondary" : ""}
             >
               Contact
             </a>
@@ -66,20 +77,20 @@ export default function DevNavBar() {
             <li key="social" role="list" dir="rtl">
               {/* li makes it a hover over instead of button*/}
               {/* <details role="list" dir="rtl"> */}
-                <summary aria-haspopup="listbox" role="link">
-                  Socials
-                </summary>
-                <ul role="listbox">
-                  {social_links.map((link) => {
-                    const { url, website } = link;
-                    return (
-                      <>
-                        <a href={"https://" + url}>{website}</a>
-                        {/*// TODO: add styling */}
-                      </>
-                    );
-                  })}
-                </ul>
+              <summary aria-haspopup="listbox" role="link">
+                Socials
+              </summary>
+              <ul role="listbox">
+                {social_links.map((link) => {
+                  const { url, website } = link;
+                  return (
+                    <>
+                      <a href={"https://" + url}>{website}</a>
+                      {/*// TODO: add styling */}
+                    </>
+                  );
+                })}
+              </ul>
               {/* </details> */}
             </li>
           )}
