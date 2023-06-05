86a90486d95528dbf408d18db2b65c8aadf64e17
Benny Sufrin <bennysuf@gmail.com>
2023-06-04 20:20:35 -0400

    add dynamic link name

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -1,17 +1,20 @@
-// import { Link } from "react-router-dom";
 export default function ProjectCard({ project }) {
   const { title, url, description } = project;
 
-  //TODO: need to check if "https://" is in the url, else add it href={"https://" + url}
-  // TODO: add dynamic name change for url
+  const { link, website} = url
+  
+  // TODO: need to check if "https://" is in the url, else add it href={"https://" + url}
+  // TODO: make URL a hash in backend with url and website
 
   return (
     <article className="card">
       <header>Title: {title}</header>
       <h4>
         Link:{" "}
-        <a href={"https://" + url} target="_blank" rel="noreferrer">
-          {url}
+        {/* <a href={"https://" + url} target="_blank" rel="noreferrer"> */}
+        <a href={"https://" + link} target="_blank" rel="noreferrer">
+          {website}
+          {/* {url} */}
         </a>
       </h4>
       <p>Description: {description}</p>
