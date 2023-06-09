afa28e659b84da7509bf89b8788e437b10081462
Benny Sufrin <bennysuf@gmail.com>
2023-06-08 21:57:00 -0400

    update relationship and valiation

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -2,17 +2,14 @@ class Developer < ApplicationRecord
     has_many :dev_projects
     has_many :projects, through: :dev_projects
     has_many :contact_forms
+    has_one :social_link
 
     has_secure_password
-
-    serialize :social_links, Array
 
     validates :password, presence: true
     validates :password_confirmation, presence: true
     validates :username, presence: true, uniqueness: true
     validates_format_of :resume, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i, allow_blank: true
-    # validates_format_of :social_links, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
-    # * to validate, maybe add row for each link? or custom validation to map through array?
     # validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true #! test 
     # validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
     # validates :email, email: {mode: :strict, require_fqdn: true}
