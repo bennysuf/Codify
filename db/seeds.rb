adf746dba6c8a334dd53da43c04a82e1578b8d35
Benny Sufrin <bennysuf@gmail.com>
2023-06-08 22:01:10 -0400

    add data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
 puts "Planting seeds"
 Developer.create(username: "benny", password: "benny", password_confirmation: "benny", email: "benny@gmail.com", public_profile: true)
 Developer.create(username: "test", password: "test", password_confirmation: "test", email: "test@aol.com", public_profile: true)
-# Developer.create(username: "", password: "", password_confirmation: "", email: "")
-# Developer.create(username: "", password: "", password_confirmation: "", email: "")
+Developer.create(username: "dad", password: "dad", password_confirmation: "dad", email: "dad@aol.com", public_profile: false)
+
+SocialLink.create(developer_id: 1)
+SocialLink.create(developer_id: 2)
+SocialLink.create(developer_id: 3)
+
 
 puts "Seeds planted"
