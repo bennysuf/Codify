cb1f5d800fb6dcb11d600ca017288751cd22976b
Benny Sufrin <bennysuf@gmail.com>
2023-06-16 13:26:54 -0400

    remove comments and unused hooks

--- client/src/components/admin/Admin.js
+++ client/src/components/admin/Admin.js
@@ -1,5 +1,5 @@
-import React, { useState, useContext } from "react";
-import { useParams, Routes, Route } from "react-router-dom";
+import React, { useContext } from "react";
+import { Routes, Route } from "react-router-dom";
 import Logout from "./Logout";
 import { UserContext } from "../App";
 import EditAdmin from "./EditAdmin";
@@ -9,57 +9,15 @@ import ProjectsPage from "./ProjectsPage";
 import CreateProfile from "./CreateProfile";
 
 export default function Admin() {
-  // const [about, setAbout] = useState(false);
-  // const [page, setPage] = useState(true);
   const { admin } = useContext(UserContext);
 
   const { username } = admin;
-
-  // function Home() {
-  //   return window.location.pathname;
-  // }
-  // console.log("URL", Home());
-
-  // function handleClick() {
-  //   setAbout(!about);
-  //   // if (window.location.pathname === "/admin/"){
-  //   //   setPage(false)
-  //   // }
-  //   //TODO: grab param, if its not admin then dont show button
-    
-  // }
-
-  // useEffect(()=> {
-  //   // window.addEventListener('popstate', (event) => {
-  //     setReload("redo");
-  //     // alert("You message");
-  //   // });
-  // },[])
 
   return (
     <div style={{marginTop: "3%"}}>
       <Logout />
       <AdminNavBar />
-      {/* <AdminNavBar profile={admin.profile} setPage={setPage} /> */}
       {username}
-      {/* {window.location.pathname === "/admin/" ? (
-        <>
-          <button onClick={handleClick} className="button">
-            See about me
-          </button>
-          <br />
-        </>
-      ) : (
-        ""
-      )}
-      {about ? (
-        <textarea
-          defaultValue={profile ? profile.about : "Add about me"}
-          style={{ height: "400px" }}
-        />
-      ) : (
-        ""
-      )} */}
       <Routes>
         <Route path="edit-admin" element={<EditAdmin />} />
         <Route path="create-profile" element={<CreateProfile />} />
