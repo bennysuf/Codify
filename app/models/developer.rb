489f959a9b68987b03f9aa26e5f427acf5c81701
Benny Sufrin <bennysuf@gmail.com>
2023-06-21 12:45:56 -0400

    add email validation

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -11,11 +11,7 @@ class Developer < ApplicationRecord
     validates :password_confirmation, presence: true
     validates :username, presence: true, uniqueness: true
     validates_format_of :resume, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i, allow_blank: true
-    # validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true #! test 
-    # validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
-    # validates :email, email: {mode: :strict, require_fqdn: true}
-    # validates :email, presence: true, email: true
-    # TODO: validate email
+    validates :email, format: {with:  /\A(.+)@(.+)\z/, message: "not valid"}, presence: true, uniqueness: true
 
     def send_welcome_email
         DeveloperMailer.welcome(self).deliver_now  
