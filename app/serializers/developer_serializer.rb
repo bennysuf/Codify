af2a6beabaf2f7b2f6efda068af4c359662f9d7e
Benny Sufrin <bennysuf@gmail.com>
2023-06-21 12:46:23 -0400

    add method for ordered projects

--- app/serializers/developer_serializer.rb
+++ app/serializers/developer_serializer.rb
@@ -1,6 +1,10 @@
 class DeveloperSerializer < ActiveModel::Serializer
-  attributes :id, :username, :password_digest, :public_profile, :email, :resume, :about
+  attributes :id, :username, :password_digest, :public_profile, :email, :resume, :about, :ordered_projects
 
   has_one :social_link
-  has_many :projects
+
+  def ordered_projects
+    object.projects.order("created_at DESC")
+  end 
+
 end
