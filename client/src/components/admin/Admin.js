ecb0e8ac471179a6de367e46b756d462c5b1b56e
Benny Sufrin <bennysuf@gmail.com>
2023-06-20 22:50:22 -0400

    clean up code, remove route

--- client/src/components/admin/Admin.js
+++ client/src/components/admin/Admin.js
@@ -1,7 +1,5 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Logout from "./Logout";
-import { UserContext } from "../App";
 import EditAdmin from "./EditAdmin";
 import NewProject from "./NewProject";
 import AdminNavBar from "./AdminNavBar";
@@ -9,15 +7,10 @@ import ProjectsPage from "./ProjectsPage";
 import CreateProfile from "./CreateProfile";
 
 export default function Admin() {
-  const { admin } = useContext(UserContext);
-
-  const { username } = admin;
 
   return (
     <div style={{marginTop: "3%"}}>
-      <Logout />
       <AdminNavBar />
-      {username}
       <Routes>
         <Route path="edit-admin" element={<EditAdmin />} />
         <Route path="create-profile" element={<CreateProfile />} />
