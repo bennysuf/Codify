0361d71aa610b93eb76186bb9b4a2645e175270c
Benny Sufrin <bennysuf@gmail.com>
2023-06-26 16:10:34 -0400

    add route and remove old routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  resources :contact_forms, only: [:new, :create ]
-  resources :dev_projects
   resources :projects
   resources :developers
+  resources :collaborations, only: [:create, :destroy]
   get "/admin", to: "developers#show"
   post "/signup", to: "developers#create"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
