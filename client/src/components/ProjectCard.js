6cc569f91b698ab41065bee50ecb46375220858f
Benny Sufrin <bennysuf@gmail.com>
2023-06-27 15:55:24 -0400

    add logic to render collaborators

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -1,4 +1,9 @@
-export default function ProjectCard({ project }) {
+import { Link } from "react-router-dom";
+import { UserContext } from "./App";
+import { useContext } from "react";
+
+export default function ProjectCard({ project, collabs }) {
+  const { currentDev, admin } = useContext(UserContext);
   const { title, url, description, linkText } = project;
 
   // TODO: need to check if "https://" is in the url, else add it href={"https://" + url}
@@ -12,6 +17,32 @@ export default function ProjectCard({ project }) {
         </a>
       </h4>
       <p>{description}</p>
+      {collabs[0] ? (
+        <nav>
+          <li key="social" role="list" dir="rtl">
+            <summary aria-haspopup="listbox" role="link">
+              Collaborators
+            </summary>
+            <ul role="listbox">
+              {collabs.map((dev) => {
+                const {dev_username, id} = dev
+                if (dev_username !== currentDev?.username || dev_username !== admin?.username) {
+                  return (
+                    <Link
+                      key={id}
+                      to={`/developer?developers=${dev_username}`}
+                    >
+                      {dev_username}
+                    </Link>
+                  );
+                }
+              })}
+            </ul>
+          </li>
+        </nav>
+      ) : (
+        <></>
+      )}
     </article>
   );
 }
