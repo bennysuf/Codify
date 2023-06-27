3c2a609c7a3cd4d9d2472983a1e437ed0609a975
Benny Sufrin <bennysuf@gmail.com>
2023-06-27 15:56:00 -0400

    add logic to render collaborators

--- client/src/components/admin/ProjectsPage.js
+++ client/src/components/admin/ProjectsPage.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { UserContext } from "../App";
 import ProjectCard from "../ProjectCard";
@@ -6,23 +6,32 @@ import EditProject from "./EditProject";
 
 export default function ProjectsPage() {
   const [projectProp, setProjectProp] = useState("");
-  const { projects, navigate } = useContext(UserContext);
+  const [projects, setProjects] = useState([])
+  const { navigate, admin } = useContext(UserContext);
 
   function handleEdit(project) {
     setProjectProp(project);
+    // ? project to edit doesnt change till page reload
+    window.scrollTo(0, 0)
     navigate(`/admin/projects-page/${project.id}`);
+
   }
+
+  useEffect(() => {
+    fetch(`/projects/${admin.id}`)
+      .then((r) => r.json())
+      .then((d) => setProjects(d));
+  }, []);
 
   const projectList = projects.map((project) => {
     return (
       <div key={project.url.link}>
-        <ProjectCard key={project.id} project={project} />
+        <ProjectCard key={project.id} project={project} collabs={project.collaborations}/>
         <br />
         <button className="button" onClick={() => handleEdit(project)}>
           Edit
         </button>
         <br />
-        {window.scrollTo(0, 0)}
       </div>
     );
   });
