df94aa9cf12dbcefa54a9a8409596f53d0b66c5c
Benny Sufrin <bennysuf@gmail.com>
2023-07-07 18:56:31 -0400

    add tags and format code

--- client/src/components/user/DevNavBar.js
+++ client/src/components/user/DevNavBar.js
@@ -21,78 +21,80 @@ export default function DevNavBar() {
   const path = window.location.pathname;
 
   return (
-    <>
-      <nav>
-        <ul style={{ marginLeft: "3%" }}>
-          <li>
-            <strong onClick={()=> navigate("/")}>Codify</strong>
-          </li>
-        </ul>
-        <ul style={{ marginRight: "7%" }}>
-          {about === "" ? (
-            <></>
-          ) : (
-            <li key="about">
-              <Link
-                to={"/developer/about?developers=" + username}
-                className={path === "/developer/about" ? "secondary" : ""}
-              >
-                About
-              </Link>
+    <body>
+      <main class="container">
+        <nav>
+          <ul style={{ marginLeft: "3%" }}>
+            <li>
+              <strong onClick={() => navigate("/")}>Codify</strong>
             </li>
-          )}
-          {!ordered_projects[0] ? (
-            <></>
-          ) : (
-            <li key="projects">
-              <Link
-                to={"/developer/projects?developers=" + username}
-                className={path === "/developer/projects" ? "secondary" : ""}
-              >
-                Projects
-              </Link>
+          </ul>
+          <ul style={{ marginRight: "7%" }}>
+            {about === "" ? (
+              <></>
+            ) : (
+              <li key="about">
+                <Link
+                  to={"/developer/about?developers=" + username}
+                  className={path === "/developer/about" ? "secondary" : ""}
+                >
+                  About
+                </Link>
+              </li>
+            )}
+            {!ordered_projects[0] ? (
+              <></>
+            ) : (
+              <li key="projects">
+                <Link
+                  to={"/developer/projects?developers=" + username}
+                  className={path === "/developer/projects" ? "secondary" : ""}
+                >
+                  Projects
+                </Link>
+              </li>
+            )}
+            {resume === "" ? (
+              <></>
+            ) : (
+              <li key="resume">
+                <Link to={"https://" + resume} target="_blank" rel="noreferrer">
+                  Resume
+                </Link>
+              </li>
+            )}
+            {socialLinks ? (
+              <li key="social" role="list" dir="rtl">
+                <summary aria-haspopup="listbox" role="link">
+                  Socials
+                </summary>
+                <ul role="listbox">
+                  {Object.entries(social_link).map((link) => {
+                    return link[1] ? (
+                      <Link
+                        key={link[0]}
+                        to={"https://" + link[1]}
+                        style={{ marginBottom: "2px", margin: "2px" }}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {link[0]}
+                      </Link>
+                    ) : (
+                      ""
+                    );
+                  })}
+                </ul>
+              </li>
+            ) : (
+              <></>
+            )}
+            <li key="home">
+              <Link to="/">Home</Link>
             </li>
-          )}
-          {resume === "" ? (
-            <></>
-          ) : (
-            <li key="resume">
-              <Link to={"https://" + resume} target="_blank" rel="noreferrer">
-                Resume
-              </Link>
-            </li>
-          )}
-          {socialLinks ? (
-            <li key="social" role="list" dir="rtl">
-              <summary aria-haspopup="listbox" role="link">
-                Socials
-              </summary>
-              <ul role="listbox">
-                {Object.entries(social_link).map((link) => {
-                  return link[1] ? (
-                    <Link
-                      key={link[0]}
-                      to={"https://" + link[1]}
-                      style={{ marginBottom: "2px", margin: "2px" }}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      {link[0]}
-                    </Link>
-                  ) : (
-                    ""
-                  );
-                })}
-              </ul>
-            </li>
-          ) : (
-            <></>
-          )}
-          <li key="home">
-            <Link to="/">Home</Link>
-          </li>
-        </ul>
-      </nav>
-    </>
+          </ul>
+        </nav>
+      </main>
+    </body>
   );
 }
