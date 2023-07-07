ddfb392f881466712fdd856d3b67bb4b4e69d2f3
Benny Sufrin <bennysuf@gmail.com>
2023-07-07 18:55:23 -0400

    move projects fetch out, format code

--- client/src/components/admin/ProjectsPage.js
+++ client/src/components/admin/ProjectsPage.js
@@ -1,32 +1,27 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { UserContext } from "../App";
 import ProjectCard from "../ProjectCard";
 import EditProject from "./EditProject";
 
-export default function ProjectsPage() {
+export default function ProjectsPage({ projects, setProjects }) {
   const [projectProp, setProjectProp] = useState("");
-  const [projects, setProjects] = useState([])
   const { navigate, admin } = useContext(UserContext);
 
   function handleEdit(project) {
     setProjectProp(project);
-    // ? project to edit doesnt change till page reload
-    window.scrollTo(0, 0)
+    window.scrollTo(0, 0);
     navigate(`/admin/projects-page/${project.id}`);
-
   }
-
-  useEffect(() => {
-    fetch(`/projects/${admin.id}`)
-      .then((r) => r.json())
-      .then((d) => setProjects(d));
-  }, []);
 
   const projectList = projects.map((project) => {
     return (
-      <div key={project.url.link}>
-        <ProjectCard key={project.id} project={project} collabs={project.collaborations}/>
+      <div key={project.id}>
+        <ProjectCard
+          key={project.id}
+          project={project}
+          collabs={project.collaborations}
+        />
         <br />
         <button className="button" onClick={() => handleEdit(project)}>
           Edit
@@ -37,14 +32,22 @@ export default function ProjectsPage() {
   });
 
   return (
-    <div style={{marginTop: "20px"}}>
-      <Routes>
-        <Route
-          path=":projectId"
-          element={<EditProject project={projectProp} />}
-        />
-      </Routes>
-      {projectList}
-    </div>
+    <body>
+      <main class="container">
+        <Routes>
+          <Route
+            path=":projectId"
+            element={
+              <EditProject
+                project={projectProp}
+                projects={projects}
+                updateProjects={setProjects}
+              />
+            }
+          />
+        </Routes>
+        {projectList}
+      </main>
+    </body>
   );
 }
