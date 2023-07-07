39f9c1fff6c54dcfd2269870471591a811d0743a
Benny Sufrin <bennysuf@gmail.com>
2023-07-07 18:52:08 -0400

    add ability to add collbaorations, add dropdown for collaborations and logic handling

--- client/src/components/admin/EditProject.js
+++ client/src/components/admin/EditProject.js
@@ -1,23 +1,57 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { UserContext } from "../App";
 
-export default function EditProject({ project }) {
-  const { projects, setProjects, navigate } = useContext(UserContext);
+export default function EditProject({ project, updateProjects, projects }) {
+
+  console.log("from edit project", projects);
+
+  const { navigate, devs, admin } = useContext(UserContext);
 
   const { projectId } = useParams();
 
-  const currentProject = projects.filter(
+  const currentProject = projects.find(
     (project) => project.id === parseInt(projectId)
   );
 
-  const { description, title, url, linkText } = currentProject[0];
+  // console.log("currebt project", currentProject)
+  // TODO: fix page reload issue
+
+  const { description, title, url, linkText, collaborations } = currentProject;
 
   const [newTitle, setNewTitle] = useState(title);
   const [newDescription, setNewDescription] = useState(description);
   const [newUrl, setNewUrl] = useState(url);
-  const [newWebText, setNewWebText] = useState(linkText ? linkText : "");
+  const [newWebText, setNewWebText] = useState(linkText);
+  const [newCollaborators, setNewCollaborators] = useState(collaborations);
   const [errors, setErrors] = useState([]);
+  const [added, setAdded] = useState("");
+
+  useEffect(() =>{
+    setNewTitle(title);
+    setNewDescription(description);
+    setNewUrl(url);
+    setNewWebText(linkText);
+    setNewCollaborators(collaborations);
+  },[project])
+
+  function handleCollabAdd(dev) {
+    if (!newCollaborators.find((user) => user === dev)) {
+      setNewCollaborators([...newCollaborators, dev]);
+      setAdded("Added");
+    }
+  }
+
+  function handleCollabRemoval(dev) {
+    setNewCollaborators(newCollaborators.filter((user) => user.id !== dev.id));
+    setAdded("Removed");
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAdded("");
+    }, 750);
+  }, [newCollaborators]);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -35,11 +69,31 @@ export default function EditProject({ project }) {
       body: JSON.stringify(update),
     }).then((r) => {
       if (r.ok) {
-        r.json().then((d) => {
+        r.json().then((updatedProject) => {
           const updated = projects.map((project) => {
-            return project.id !== d.id ? project : d;
+            return project.id !== updatedProject.id ? project : updatedProject;
           });
-          setProjects(updated);
+          if (newCollaborators[0]) {
+            newCollaborators.forEach((dev) => {
+              fetch("/collaborations", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                  dev,
+                  project_id: updatedProject.id,
+                }),
+              }).then((r) => {
+                if (r.ok) {
+                  r.json().then((d) => {
+                    updatedProject.collaborations = [d];
+                    updateProjects(updated);
+                  });
+                }
+              });
+            });
+          } else {
+            updateProjects(updated);
+          }
           navigate("/admin/projects-page");
         });
       } else {
@@ -61,7 +115,7 @@ export default function EditProject({ project }) {
       .then((r) => r.json())
       .then(() => {
         const updated = projects.filter((project) => project.id !== item.id);
-        setProjects(updated);
+        updateProjects(updated);
         navigate("/admin/projects-page");
       });
   }
@@ -96,12 +150,55 @@ export default function EditProject({ project }) {
             onChange={(e) => setNewDescription(e.target.value)}
             style={{ height: "100px" }}
           />
+          <div class="grid">
+            <details role="list">
+              <summary aria-haspopup="listbox" role="button">
+                Collaborators
+              </summary>
+              <ul role="listbox">
+                {newCollaborators.map((dev) => {
+                  return (
+                    <li key={dev.id} onClick={() => handleCollabRemoval(dev)}>
+                      Remove {dev.dev_username}
+                    </li>
+                  );
+                })}
+              </ul>
+            </details>
+            <details role="list">
+              <summary aria-haspopup="listbox" role="button">
+                {/* //TODO: add search bar, dropdown only shows search.includes */}
+                Add collaborators
+              </summary>
+              <ul role="listbox">
+                {devs.map((dev) => {
+                  if (dev.username !== admin.username) {
+                    return (
+                      <li
+                        key={dev.id}
+                        onClick={() => handleCollabAdd(dev.username)}
+                      >
+                        {dev.username}
+                      </li>
+                    );
+                  }
+                })}
+              </ul>
+            </details>
+          </div>
         </div>
-        <button type="submit" className="button">Submit</button>
-        <br/>
-        <button type="button" className="button" onClick={() => handleDelete(project)}>
+        <button type="submit" className="button">
+          Submit
+        </button>
+        <br />
+        <button
+          type="button"
+          className="button"
+          onClick={() => handleDelete(project)}
+        >
           Delete project
         </button>
+        <div style={{ textAlign: "center" }}>{added}</div>
       </form>
     </>
   );
