e68866b30b5d4270114f2d78329f6f267d0fd8f9
Benny Sufrin <bennysuf@gmail.com>
2023-07-12 16:06:33 -0400

    add context for admin

--- client/src/components/admin/Admin.js
+++ client/src/components/admin/Admin.js
@@ -1,29 +1,39 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect, createContext } from "react";
 import { Routes, Route } from "react-router-dom";
 import EditAdmin from "./EditAdmin";
 import NewProject from "./NewProject";
 import AdminNavBar from "./AdminNavBar";
 import ProjectsPage from "./ProjectsPage";
 
-export default function Admin({admin}) {
-  const [projects, setProjects] = useState([])
+export const AdminContext = createContext(null);
+
+export default function Admin({ admin }) {
+  const [projects, setProjects] = useState([]);
+  const [reload, setReload] = useState("");
 
   useEffect(() => {
     fetch(`/projects/${admin.id}`)
       .then((r) => r.json())
       .then((d) => setProjects(d));
-  }, []);
+  }, [reload]);
 
   return (
-    <div>
+    <AdminContext.Provider
+      value={{
+        projects,
+        setProjects,
+        setReload,
+      }}
+    >
       <div className="hide-on-mobile">
-      <AdminNavBar />
+        <AdminNavBar />
       </div>
       <Routes>
         <Route path="edit-admin" element={<EditAdmin />} />
-        <Route path="new-project" element={<NewProject projects={projects} setProjects={setProjects}/>} />
-        <Route path="projects-page/*" element={<ProjectsPage projects={projects} setProjects={setProjects}/>} />
+        <Route path="new-project" element={<NewProject />} />
+        <Route path="projects-page/*" element={<ProjectsPage />} />
+        {/* <Route path="projects-page/*" element={<ProjectsPage projects={projects} setProjects={setProjects}/>} /> */}
       </Routes>
-    </div>
+    </AdminContext.Provider>
   );
 }
