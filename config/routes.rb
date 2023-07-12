2bf7fdd9c81722d30ee09f8fea5795ec120a0d5d
Benny Sufrin <bennysuf@gmail.com>
2023-07-12 18:46:44 -0400

    add custom route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post "/signup", to: "developers#create"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+  get "get-project/:id", to: "projects#show_project"
 
   # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
