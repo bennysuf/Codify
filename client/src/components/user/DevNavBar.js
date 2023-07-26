e01fea702b89b993d30aa64427a43bb4f8e1d25a
Benny Sufrin <bennysuf@gmail.com>
2023-07-26 16:56:12 -0400

    make navbar dynamic based on screen size

--- client/src/components/user/DevNavBar.js
+++ client/src/components/user/DevNavBar.js
@@ -20,79 +20,101 @@ export default function DevNavBar() {
 
   const path = window.location.pathname;
 
+  const navbar = (
+    <>
+      {about === "" ? (
+        <></>
+      ) : (
+        <li key="about">
+          <Link
+            to={"/developer/about?developers=" + username}
+            className={path === "/developer/about" ? "secondary" : ""}
+          >
+            About
+          </Link>
+        </li>
+      )}
+      {!ordered_projects[0] ? (
+        <></>
+      ) : (
+        <li key="projects">
+          <Link
+            to={"/developer/projects?developers=" + username}
+            className={path === "/developer/projects" ? "secondary" : ""}
+          >
+            Projects
+          </Link>
+        </li>
+      )}
+      {resume === "" ? (
+        <></>
+      ) : (
+        <li key="resume">
+          <Link to={"https://" + resume} target="_blank" rel="noreferrer">
+            Resume
+          </Link>
+        </li>
+      )}
+      {socialLinks ? (
+        <li key="social" role="list" dir="rtl">
+          <summary aria-haspopup="listbox" role="link">
+            Socials
+          </summary>
+          <ul role="listbox">
+            {Object.entries(social_link).map((link) => {
+              return link[1] ? (
+                <Link
+                  key={link[0]}
+                  to={"https://" + link[1]}
+                  style={{ marginBottom: "2px", margin: "2px" }}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {link[0]}
+                </Link>
+              ) : (
+                ""
+              );
+            })}
+          </ul>
+        </li>
+      ) : (
+        <></>
+      )}
+      <li key="home">
+        <Link to="/">Home</Link>
+      </li>
+    </>
+  );
+
+  const desktop = (
+    <ul style={{ marginRight: "3%" }} className="hide-on-mobile">
+      {navbar}
+    </ul>
+  );
+
+  const mobile = (
+    <li key="drop" role="list" dir="rtl" className="hide-on-desktop">
+      <summary aria-haspopup="listbox" role="link">
+        ☰
+      </summary>
+      <ul role="listbox" style={{ marginRight: "3%" }}>
+        {navbar}
+      </ul>
+    </li>
+  );
+
   return (
     <body>
       <main class="container">
         <nav>
-          <ul style={{ marginLeft: "3%" }}>
+          <ul style={{ marginLeft: "5%" }}>
             <li>
               <strong onClick={() => navigate("/")}>Codify</strong>
             </li>
           </ul>
-          <ul style={{ marginRight: "7%" }}>
-            {about === "" ? (
-              <></>
-            ) : (
-              <li key="about">
-                <Link
-                  to={"/developer/about?developers=" + username}
-                  className={path === "/developer/about" ? "secondary" : ""}
-                >
-                  About
-                </Link>
-              </li>
-            )}
-            {!ordered_projects[0] ? (
-              <></>
-            ) : (
-              <li key="projects">
-                <Link
-                  to={"/developer/projects?developers=" + username}
-                  className={path === "/developer/projects" ? "secondary" : ""}
-                >
-                  Projects
-                </Link>
-              </li>
-            )}
-            {resume === "" ? (
-              <></>
-            ) : (
-              <li key="resume">
-                <Link to={"https://" + resume} target="_blank" rel="noreferrer">
-                  Resume
-                </Link>
-              </li>
-            )}
-            {socialLinks ? (
-              <li key="social" role="list" dir="rtl">
-                <summary aria-haspopup="listbox" role="link">
-                  Socials
-                </summary>
-                <ul role="listbox">
-                  {Object.entries(social_link).map((link) => {
-                    return link[1] ? (
-                      <Link
-                        key={link[0]}
-                        to={"https://" + link[1]}
-                        style={{ marginBottom: "2px", margin: "2px" }}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        {link[0]}
-                      </Link>
-                    ) : (
-                      ""
-                    );
-                  })}
-                </ul>
-              </li>
-            ) : (
-              <></>
-            )}
-            <li key="home">
-              <Link to="/">Home</Link>
-            </li>
-          </ul>
+          {desktop}
+          {mobile}
         </nav>
       </main>
     </body>
